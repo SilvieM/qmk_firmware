66e030c81700944cbabf70ba9ea0d094e2e9322c
Silvie Müller <silvie.mueller@web.de>
2024-06-21 14:38:34 +0200

    symbole nach oben

--- keyboards/silvies_dactyl/keymaps/default/keymap.c
+++ keyboards/silvies_dactyl/keymaps/default/keymap.c
@@ -9,13 +9,14 @@
 
 enum custom_keycodes {
   QUOTE = SAFE_RANGE,
-  BACKTICK
+  BACKTICK,
+  DBLQUOT
 };
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC,
+        QK_GESC,  KC_QUES,  DBLQUOT,  KC_PIPE,    KC_LCBR, KC_RCBR,                                                  KC_LABK,    KC_RABK,    KC_COLN,    KC_LPRN,    KC_RPRN,     KC_BSPC,
         KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                                                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,  US_UDIA, //ü
         KC_MINS,  KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                                                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O,     US_ADIA,//ä
         OSM(MOD_LSFT), KC_Z,KC_X,   KC_C,    KC_D,    KC_V,                                                     KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  US_ODIA,//ö
@@ -25,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [2] = LAYOUT(
         QK_BOOT,    KC_F1,    KC_F2,    KC_F3,      KC_F4,       KC_F5,                                     KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
-        QK_REBOOT,    KC_EXLM,  KC_AT,    KC_HASH,    KC_DLR,      KC_PERC,                                   KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_F12,
+        QK_REBOOT,  KC_EXLM,  KC_AT,    KC_HASH,    KC_DLR,      KC_PERC,                                   KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_F12,
         KC_0,       KC_1,     KC_2,     KC_3,       KC_4,        KC_5,                                      KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_TRNS,
         KC_TRNS, KC_TILD,    KC_GRV,    KC_QUOT,    KC_TRNS,    KC_TRNS,                                   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_BSLS,    KC_TRNS,
         KC_TRNS, KC_TRNS,   KC_TRNS,   KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS,                 KC_TRNS, KC_PIPE,   BACKTICK, KC_TRNS,
@@ -54,6 +55,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
     case BACKTICK:
       if (record->event.pressed) {
         tap_code(KC_GRAVE);
+        tap_code(KC_SPC);
+      }
+      return false;
+    case DBLQUOT:
+      if (record->event.pressed) {
+        tap_code16(LSFT(KC_QUOTE));
         tap_code(KC_SPC);
       }
       return false;
